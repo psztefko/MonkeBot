9fd0031f8537206dfbbddf1a8ba9f45685fdd66c
Piotrek <p.sztefko@gmail.com>
2021-05-21 17:05:10 +0200

    token is now hidden so u can't hijack my bot ಠ_ಠ

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.GenerateMessage import GenerateMessage
 from discord.ext.commands import Bot
 from random import randint
 from pathlib import Path
+from env import TOKEN
 import discord
 import time
 
@@ -26,7 +27,7 @@ async def on_message(message):
     messages = randint(2,3)
     readOutLoud = False
 
-    if message.channel.name == 'test':
+    if message.channel.name == 'communist-manifesto':
         if user_message.lower() == 'super':
             await message.channel.send(f'dzieki {username}')
         elif user_message.lower() == 'czytaj':
@@ -38,6 +39,6 @@ async def on_message(message):
             await message.channel.send(file=discord.File(Path(msg.get_image())))
             time.sleep(1.0)
 
-client.run('ODQ1MDI0MzI5NzU4MTQ2NTYx.YKa8PA.syIKVeLwLkMwGHp1QJqK5rgeoFM')
+client.run(TOKEN)
 
 
